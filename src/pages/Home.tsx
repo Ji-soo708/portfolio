a973ad6ee4f5ffd1b4559348e4778d4c923e5cca
Ji-soo708 <dlawotn3@naver.com>
2024-08-01 21:35:35 +0900

    feat: add EtcScrollableBoxes component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import Toolbar from '../components/Toolbar';
 import Header from '../components/MainHeader';
 import ProjectScrollableBoxes from '../components/ProjectScrollableBoxes';
 import BlogScrollableBoxes from '../components/BlogScrollableBoxes';
+import SkillScrollableBoxes from '../components/SkillScrollableBoxes';
+import EtcScrollableBoxes from '../components/EtcScrollableBoxes';
 
 function Home() {
   return (
@@ -32,6 +34,28 @@ function Home() {
           </div>
         </div>
         <BlogScrollableBoxes />
+        <div className='flex flex-col items-start'>
+          <div className="flex space-x-2 ml-5">
+            <p className="text-2xl font-bold text-[#1D1D1F] mt-20">
+              Skill.
+            </p>
+            <p className="text-2xl font-bold text-[#6E6E73] mt-20">
+              아이디어를 현실로 실현하는 기술.
+            </p>
+          </div>
+        </div>
+        <SkillScrollableBoxes />
+        <div className='flex flex-col items-start'>
+          <div className="flex space-x-2 ml-5">
+            <p className="text-2xl font-bold text-[#1D1D1F] mt-20">
+              Etcg.
+            </p>
+            <p className="text-2xl font-bold text-[#6E6E73] mt-20">
+              성장과 배움의 순간들.
+            </p>
+          </div>
+        </div>
+        <EtcScrollableBoxes />
       </div>
     </div>
   );
