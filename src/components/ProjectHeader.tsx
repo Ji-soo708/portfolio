f009a6a69e99cf9640f2f2a99705dbf8e0b0b9d2
Ji-soo708 <dlawotn3@naver.com>
2024-08-01 22:14:46 +0900

    feat: add ProjectHeader component

---
+++ src/components/ProjectHeader.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import FolderIcon from '../assets/svg/folder.svg';
+
+interface ProjectHeaderProps {
+  imageSrc: string;
+  text: string;
+}
+
+const ProjectHeader: React.FC<ProjectHeaderProps> = ({ imageSrc, text }) => {
+  const combinedText = `/projects/${text}`;
+
+  return (
+    <div className="flex flex-col my-4">
+      <div className="flex items-center text-xl mb-2 ml-14 py-5 underline">
+        <img src={FolderIcon} alt="Folder" className="w-6 h-6 mr-2" />
+        {combinedText}
+      </div>
+      <div className="flex justify-center mt-14">
+        <img src={imageSrc} alt="Project" className="w-[1070px] h-[590px] object-cover" />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectHeader;
--- src/pages/Bibbi.tsx
+++ src/pages/Bibbi.tsx
@@ -1,4 +1,13 @@
+import Toolbar from '../components/Toolbar';
+import BibbiImage from '../assets/svg/bibbi-project.svg';
+import ProjectHeader from '../components/ProjectHeader';
+
 function Bibbi() {
-  return <div>Bibbi</div>;
+  return (
+  <div>
+    <Toolbar />
+    <ProjectHeader imageSrc={BibbiImage} text="bibbi" />
+  </div>
+  );
 }
 export default Bibbi;
