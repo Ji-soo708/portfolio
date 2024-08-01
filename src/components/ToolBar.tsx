dbbb174e8e2ed69604eb63db6f2035ac0c683bb4
Ji-soo708 <dlawotn3@naver.com>
2024-08-01 16:15:04 +0900

    feat: add ToolBar component

---
+++ src/components/ToolBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Toolbar: React.FC = () => {
+  return (
+    <div className="w-full max-w-[1512px] h-[40px] bg-black/80 mx-auto flex items-center px-4">
+      <div className="flex-grow" />
+      <div className="flex space-x-[34px] pr-[117px]">
+        <a href="https://github.com" className="text-white">Home</a>
+        <a href="https://github.com" className="text-white">Github</a>
+        <a href="https://github.com" className="text-white">Blog</a>
+        <a href="https://github.com" className="text-white">LinkedIn</a>
+      </div>
+    </div>
+  );
+};
+
+export default Toolbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import Toolbar from '../components/ToolBar';
 
 function Home() {
-  return (
-    <div className="p-4">
-      <h1 className="text-red-500 text-3xl">Tailwind CSS Test</h1>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
-        Button
-      </button>
-    </div>
-  );
-}
+    return (
+      <div>
+        <Toolbar />
+      </div>
+    );
+  }
+  
 
 export default Home;
