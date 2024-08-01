2506a468d0951cfbd6a07e17ae937e63bdd66257
Ji-soo708 <dlawotn3@naver.com>
2024-08-01 16:06:13 +0900

    chore: install Tailwind CSS

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-red-500 text-3xl">Tailwind CSS Test</h1>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['San Francisco', 'Apple SD Gothic Neo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
