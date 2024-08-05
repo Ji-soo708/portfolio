f692c007f0f80b32ce8b361f27c6c9a8b8ba9a55
Ji-soo708 <dlawotn3@naver.com>
2024-08-05 18:21:26 +0900

    refact: adjust font size in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,11 +17,14 @@ function Home() {
       <Toolbar />
       <div className="ml-0 md:ml-12 lg:ml-24 xl:ml-48">
         <Header />
+
         {/* Project Section */}
         <div className="flex flex-col items-start mt-3 lg:mt-5 xl:mt-5">
           <div className="flex space-x-2 ml-5">
-            <p className="text-2xl font-bold text-[#1D1D1F] mt-20">Project.</p>
-            <p className="text-2xl font-bold text-[#6E6E73] mt-20">
+            <p className="text-xl md:text-2xl font-bold text-[#1D1D1F] mt-10 md:mt-20">
+              Project.
+            </p>
+            <p className="text-base md:text-2xl font-bold text-[#6E6E73] mt-10 md:mt-20">
               사용자에게 가치를 제공하는 서비스.
             </p>
           </div>
@@ -31,8 +34,10 @@ function Home() {
         {/* Blog Section */}
         <div className="flex flex-col items-start">
           <div className="flex space-x-2 ml-5">
-            <p className="text-2xl font-bold text-[#1D1D1F] mt-20">Dev-Blog.</p>
-            <p className="text-2xl font-bold text-[#6E6E73] mt-20">
+            <p className="text-xl md:text-2xl font-bold text-[#1D1D1F] mt-10 md:mt-20">
+              Dev-Blog.
+            </p>
+            <p className="text-base md:text-2xl font-bold text-[#6E6E73] mt-10 md:mt-20">
               기술적 고민을 담은 글.
             </p>
           </div>
@@ -42,8 +47,10 @@ function Home() {
         {/* Skill Section */}
         <div className="flex flex-col items-start">
           <div className="flex space-x-2 ml-5">
-            <p className="text-2xl font-bold text-[#1D1D1F] mt-20">Skill.</p>
-            <p className="text-2xl font-bold text-[#6E6E73] mt-20">
+            <p className="text-xl md:text-2xl font-bold text-[#1D1D1F] mt-10 md:mt-20">
+              Skill.
+            </p>
+            <p className="text-base md:text-2xl font-bold text-[#6E6E73] mt-10 md:mt-20">
               아이디어를 현실로 실현하는 기술.
             </p>
           </div>
@@ -53,8 +60,10 @@ function Home() {
         {/* Etc Section */}
         <div className="flex flex-col items-start">
           <div className="flex space-x-2 ml-5">
-            <p className="text-2xl font-bold text-[#1D1D1F] mt-20">Etc.</p>
-            <p className="text-2xl font-bold text-[#6E6E73] mt-20">
+            <p className="text-xl md:text-2xl font-bold text-[#1D1D1F] mt-10 md:mt-20">
+              Etc.
+            </p>
+            <p className="text-base md:text-2xl font-bold text-[#6E6E73] mt-10 md:mt-20">
               성장과 배움의 순간들.
             </p>
           </div>
