11ddb4b35e88968a9c13a7a2a3edd49612e9ac1e
Ji-soo708 <dlawotn3@naver.com>
2024-08-05 19:24:17 +0900

    refact: optimize project image display

--- src/components/ProjectHeader.tsx
+++ src/components/ProjectHeader.tsx
@@ -9,11 +9,14 @@ const ProjectHeader: React.FC<ProjectHeaderProps> = ({ imageSrc, text }) => {
   return (
     <div className="flex flex-col my-4 px-4 md:px-0">
       <div className="flex justify-center mt-8 md:mt-14">
-        <img
-          src={imageSrc}
-          alt="Project"
+        <object
+          data={imageSrc}
+          type="image/svg+xml"
+          aria-label={text}
           className="w-full md:w-[910px] h-auto md:h-[495px] object-contain rounded-[15px] md:rounded-[25px]"
-        />
+        >
+          <img src={imageSrc} alt={text} className="w-full h-auto" />
+        </object>
       </div>
     </div>
   );
